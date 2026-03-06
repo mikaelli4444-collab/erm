fbec28e4c5095596af7cad7f28067e72e3d9ca6b
Toulousegg <franklin.m.mogollon@gmail.com>
2026-03-06 17:43:11 -0300

    tls & ssl fixed

--- utilities/net/autorouter.py
+++ utilities/net/autorouter.py
@@ -12,53 +12,8 @@ def use_autorouter(
         prefix:str = "",
         exclude:list[str]=[]
     ):
-    
-    """
-    Creates an automatic router that serves HTML templates based on the requested path.
 
-    ### Args
-        templates:
-            Instance of `Jinja2Templates` used to load and render templates.
-
-        prefix:
-            Prefix applied to the template
-            directory structure.
-
-        exclude:
-            List of path that should not be handled by the autorouter. 
-            This will cause HTTP 404 error.
-
-    ### Example
-
-    Instead of defining a route for every template:
-
-    ```python
-    home_router = APIRouter(prefix="/home", tags=["home"])
-
-    @home_router.get("/login")
-    def login_page(request: Request):
-        return templates.TemplateResponse("home/login.html", {"request": request})
-
-    @home_router.get("/signup")
-    def signup_page(request: Request):
-        return templates.TemplateResponse("home/signup.html", {"request": request})
-
-    @home_router.get("/verify-email")
-    def verify_email_page(request: Request):
-        return templates.TemplateResponse("home/verify_email.html", {"request": request})
-    ```
-
-    You can generate the router automatically:
-
-    ```python
-    home_router = APIRouter(prefix="/home", tags=["home"])
-    use_autorouter(home_router, templates, prefix="/home")
-    ```
-
-    Requests such as /home/login, /home/signup, or /home/verify-email
-    will automatically render their corresponding templates inside
-    templates/home/.
-    """
+    """ Auto router.get for templates """
 
     @router.get("/{path:path}")
     def autorouter(request:Request, path:str = ""):
